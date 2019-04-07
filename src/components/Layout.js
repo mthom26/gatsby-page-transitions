127d1770d533d881e463a10bc975891b4c680a1b
mthom26 <mthom2626@gmail.com>
2019-04-08 00:31:24 +0100

    Add index.css

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -2,6 +2,8 @@ import React from 'react';
 
 import Nav from './Nav';
 
+import '../index.css';
+
 const Layout = ({ children }) => {
   return (
     <div>
--- src/components/Nav.js
+++ src/components/Nav.js
@@ -3,10 +3,10 @@ import { Link } from 'gatsby';
 
 const Nav = () => {
   return (
-    <nav>
-      <Link to="/">Index</Link>
-      <Link to="/page-2">Page 2</Link>
-      <Link to="/page-3">Page 3</Link>
+    <nav className="nav">
+      <Link to="/" className="navLink">Index</Link>
+      <Link to="/page-2" className="navLink">Page 2</Link>
+      <Link to="/page-3" className="navLink">Page 3</Link>
     </nav>
   )
 };
