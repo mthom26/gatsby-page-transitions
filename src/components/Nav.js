9c2c9d6619439dceec4a65f6d6b2637793955551
mthom26 <mthom2626@gmail.com>
2019-04-08 18:35:19 +0100

    Add page transitions and animejs

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,12 +1,55 @@
 import React from 'react';
-import { Link } from 'gatsby';
+import TransitionLink from 'gatsby-plugin-transition-link';
+import animejs from 'animejs';
 
 const Nav = () => {
   return (
     <nav className="nav">
-      <Link to="/" className="navLink">Index</Link>
-      <Link to="/page-2" className="navLink">Page 2</Link>
-      <Link to="/page-3" className="navLink">Page 3</Link>
+      <TransitionLink
+        to="/"
+        className="navLink"
+        exit={{
+          length: 6.0,
+          trigger: ({ node, e, exit, entry }) => {
+            // Get the child node so the entire Layout does not animate
+            const item = node.querySelector('.layout');
+            console.log(item)
+            return animejs({
+              targets: [item],
+              duration: 3000,
+              translateX: ['0%', '10%'],
+              easing: 'easeInOutCubic'
+            })
+          }
+        }}
+        entry={{
+          delay: 3.0,
+          length: 6.0,
+          trigger: ({ node, e, exit, entry }) => {
+            const item = node.querySelector('.layout');
+            return animejs({
+              targets: [item],
+              duration: 3000,
+              translateX: ['-10%', '0%'],
+              easing: 'easeInOutCubic'
+            })
+          }
+        }}
+      >
+        Index
+      </TransitionLink>
+      <TransitionLink
+        to="/page-2"
+        className="navLink"
+      >
+        Page 2
+      </TransitionLink>
+      <TransitionLink
+        to="/page-3"
+        className="navLink"
+      >
+        Page 3
+      </TransitionLink>
     </nav>
   )
 };
