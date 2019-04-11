63cdd930550642f7a8681e3d0144740f7527ef29
mthom26 <mthom2626@gmail.com>
2019-04-11 10:35:31 +0100

    Add transitions to all pages

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -40,12 +40,64 @@ const Nav = () => {
       <TransitionLink
         to="/page-2"
         className="navLink"
+        exit={{
+          length: 2.0,
+          trigger: ({ node, e, exit, entry }) => {
+            // Get the child node so the entire Layout does not animate
+            const item = node.querySelector('.layout');
+            return animejs({
+              targets: [item],
+              duration: 2000,
+              translateX: ['0%', '80%'],
+              easing: 'easeInOutCubic'
+            })
+          }
+        }}
+        entry={{
+          delay: 0.5,
+          length: 2.0,
+          trigger: ({ node, e, exit, entry }) => {
+            const item = node.querySelector('.layout');
+            return animejs({
+              targets: [item],
+              duration: 2000,
+              translateX: ['-80%', '0%'],
+              easing: 'easeInOutCubic'
+            })
+          }
+        }}
       >
         Page 2
       </TransitionLink>
       <TransitionLink
         to="/page-3"
         className="navLink"
+        exit={{
+          length: 2.0,
+          trigger: ({ node, e, exit, entry }) => {
+            // Get the child node so the entire Layout does not animate
+            const item = node.querySelector('.layout');
+            return animejs({
+              targets: [item],
+              duration: 2000,
+              translateX: ['0%', '80%'],
+              easing: 'easeInOutCubic'
+            })
+          }
+        }}
+        entry={{
+          delay: 0.5,
+          length: 2.0,
+          trigger: ({ node, e, exit, entry }) => {
+            const item = node.querySelector('.layout');
+            return animejs({
+              targets: [item],
+              duration: 2000,
+              translateX: ['-80%', '0%'],
+              easing: 'easeInOutCubic'
+            })
+          }
+        }}
       >
         Page 3
       </TransitionLink>
